c1495b4c537f7c526784a7c7e15d2abb2b8ce070
ericli0702 <smiledream0702@gmail.com>
2021-11-11 03:53:33 +0800

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,6 @@
         <h1>ARE YOU INTERESTED?</h1>
       </div> -->
     </section>
-<<<<<<< HEAD
     <v-container>
       <section class="home-section d-flex">
         <v-spacer></v-spacer>
@@ -24,23 +23,6 @@
         <div class="w-30 p-5">
           <h2 class="">ABOUT US</h2>
           <p class="">
-=======
-    <section class="home-section">
-      <v-row>
-        <v-col class="float-right">
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-img
-            max-height="400"
-            max-width="450"
-            src="../assets/image/Rectangle 12.png"
-          ></v-img>
-          <v-spacer></v-spacer>
-        </v-col>
-        <v-col>
-          <h2>About Us</h2>
-          <p>
->>>>>>> 7c40c7ec101d17da2520afb6c3b183cb5ebc6f4e
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
