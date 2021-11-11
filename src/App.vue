bab79569c193f2006ee577f2acd60a711920cf71
rockstar0711 <happpybopy123@outlook.com>
2021-11-11 17:01:22 +0800

    main components

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar app class="app-header">
-      <v-row>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <router-link :to="{ path: 'home' }" class="mr-5 nav-link"
-          >Home</router-link
-        >
-        <router-link :to="{ path: 'rental' }" class="mr-5 nav-link"
-          >Rentals</router-link
-        >
-        <router-link :to="{ path: 'charter' }" class="mr-5 nav-link"
-          >Charter</router-link
-        >
-        <router-link :to="{ path: 'about' }" class="mr-5 nav-link"
-          >About Us</router-link
-        >
-        <router-link :to="{ path: 'contact' }" class="mr-5 nav-link"
-          >Contact Us</router-link
-        >
-        <v-spacer></v-spacer>
-        <router-link :to="{ path: 'login' }" class="mr-5 nav-link">Log In</router-link>
-        <router-link :to="{ path: 'register' }" class="mr-5 nav-link">Register</router-link>
-      </v-row>
-    </v-app-bar>
-
+    <Header />
     <v-main>
       <router-view></router-view>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import "@/assets/css/main.css";
 export default {
+  components: {
+    Header,
+    Footer
+  },
   name: "App",
   data: () => ({
     //
---
+++ src/components/GoTop/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <a
+    v-if="show"
+    class="back2top-btn"
+    href="#"
+    data-aos="fade-up"
+    data-aos-easing="linear"
+  >
+    <v-btn fab dark color="#7A8981">
+      <v-icon>mdi-navigation</v-icon>
+    </v-btn>
+  </a>
+</template>
+
+<script>
+import AOS from "aos";
+import "aos/dist/aos.css";
+export default {
+  data: () => ({
+    windowTop: 0,
+    show: false,
+  }),
+  mounted() {
+    AOS.init();
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      this.windowTop =
+        window.top.scrollY; /* or: e.target.documentElement.scrollTop */
+      if (this.windowTop > 800) {
+        this.show = true;
+      } else {
+        this.show = false;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
