d5be2fe0d0b5626b195c1d9748b93e892593850d
ericli0702 <smiledream0702@gmail.com>
2021-11-12 07:11:31 +0800

    chater payload

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import Charter from '../views/Charter.vue';
 
 Vue.use(Router);
 
@@ -28,6 +29,11 @@ export const router = new Router({
       component: Register,
       name: "register",
     },
+    {
+      path:'/charter',
+      component:Charter,
+      name:'charter'
+    }
   ],
 });
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,8 @@ Vue.use(Vuex);
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 axios.defaults.headers.common["authorization"] =
   "Bearer " + localStorage.getItem("authData")?.accessToken;
-axios.defaults.headers.common["Content-Type"] = "application/json";
-axios.defaults.headers.common["Accept"] = "application/json";
+// axios.defaults.headers.common["Content-Type"] = "application/json";
+// axios.defaults.headers.common["Accept"] = "application/json";
 //to handle state
 export default new Vuex.Store({
   state: {
@@ -31,11 +31,15 @@ export default new Vuex.Store({
         localStorage.setItem("authData", JSON.stringify(res.data));
         localStorage.setItem("isAuthenticated", true);
       });
-      // console.log(commit,payload)
-      // axios.get('/').then(res=>{
-      //     console.log(res)
-      // })
     },
+    signUp({commit}, payload){
+      axios.post('/auth/signup',payload).then(res=>{
+        console.log(res)
+        commit("setAuthData",res.data)
+        localStorage.setItem("authData", JSON.stringify(res.data));
+        localStorage.setItem("isAuthenticated", true);
+      })
+    }
   },
 
   //to handle mutations
