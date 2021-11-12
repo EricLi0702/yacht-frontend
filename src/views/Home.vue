92ef5f4b943263e303e294e7cd472bc1b0c71f3d
rockstar0711 <happpybopy123@outlook.com>
2021-11-13 00:15:27 +0800

    boat card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -90,23 +90,142 @@
         <v-col cols="12" md="6" class="pa-md-10">
           <v-img
             class="w-100 h-100"
-            src="../assets/image/Rectangle 12.png"
+            src="../assets/image/home/about-us.png"
           ></v-img>
         </v-col>
-        <v-col cols="12" md="6" class="pa-md-10">
+        <v-col cols="12" md="6" class="pa-md-10 d-flex align-center">
           <div>
-            <p class="text-md-h3 text-h4 font-weight-black py-3">ABOUT US</p>
+            <p class="text-md-h4 text-h5 font-weight-black py-3">ABOUT US</p>
             <p class="grey--text text--darken-1 line-height-30">
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout. The
               point of using Lorem Ipsum is that it has a more-or-less normal
               distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Lorem Ipsum
-              is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled
+              content here', making it look like readable English. <br />
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled
             </p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row>
+        <p class="text-md-h4 text-h5 font-weight-black text-uppercase">
+          Yachts
+        </p>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6" class="pa-md-3 pa-2">
+          <div class="w-100 h-100">
+            <v-hover v-slot="{ hover }">
+              <v-card :elevation="hover ? 3 : 0" :class="{ 'on-hover': hover }">
+                <v-img src="../assets/image/home/yacht1.png">
+                  <div class="position-relative h-100 w-100 d-flex align-end">
+                    <div class="px-md-5 px-2 pb-5 white--text">
+                      <p class="text-h5 font-weight-bold text-left">
+                        Speed Boat 10
+                      </p>
+                      <div class="d-flex align-end">
+                        <v-icon color="white" class="pr-3"
+                          >mdi-sail-boat</v-icon
+                        >
+                        <p class="mb-0" style="width: 130px">Length</p>
+                        <p
+                          class="mb-0 text-body-1 font-weight-bold font-italic"
+                        >
+                          100 Meter
+                        </p>
+                      </div>
+                      <div class="d-flex align-end py-3">
+                        <v-icon color="white" class="pr-3">mdi-tag</v-icon>
+                        <p class="mb-0" style="width: 130px">Price per Week</p>
+                        <p
+                          class="mb-0 text-body-1 font-weight-bold font-italic"
+                        >
+                          160$
+                        </p>
+                      </div>
+                      <div class="d-flex align-end">
+                        <v-icon color="white" class="pr-3"
+                          >mdi-calendar-month</v-icon
+                        >
+                        <p class="mb-0" style="width: 130px">Built Date</p>
+                        <p
+                          class="mb-0 text-body-1 font-weight-bold font-italic"
+                        >
+                          10-Oct-2016
+                        </p>
+                      </div>
+                    </div>
+
+                    <v-btn
+                      :class="{ 'hover-show-btns': hover }"
+                      color="rgba(255, 255, 255, 0)"
+                      icon
+                      large
+                      class="position-absolute self-align-justify-center"
+                    >
+                      <v-icon
+                        :class="{ 'hover-show-btns': hover }"
+                        color="rgba(255, 255, 255, 0)"
+                        size="40"
+                      >
+                        mdi-arrow-right
+                      </v-icon>
+                    </v-btn>
+                  </div>
+                </v-img>
+              </v-card>
+            </v-hover>
+            <!-- <v-img class="w-100 h-100" src="../assets/image/home/yacht1.png">
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="
+                      d-flex
+                      transition-fast-in-fast-out
+                      v-card--reveal
+                      justify-center
+                      align-center
+                    "
+                    style="height: 100%"
+                  >
+                    <v-btn dark fab small color="yellow accent-4">
+                      <v-icon dark>mdi-arrow-right</v-icon>
+                    </v-btn>
+                  </div>
+                  <div>
+                    <p>sammie</p>
+                  </div>
+                </v-expand-transition>
+              </v-img> -->
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" class="pa-md-3 pa-2">
+          <div class="w-100 h-100">
+            <v-img
+              class="w-100 h-100"
+              src="../assets/image/home/yacht1.png"
+            ></v-img>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" class="pa-md-3 pa-2">
+          <div class="w-100 h-100">
+            <v-img
+              class="w-100 h-100"
+              src="../assets/image/home/yacht1.png"
+            ></v-img>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" class="pa-md-3 pa-2">
+          <div class="w-100 h-100">
+            <v-img
+              class="w-100 h-100"
+              src="../assets/image/home/yacht1.png"
+            ></v-img>
           </div>
         </v-col>
       </v-row>
